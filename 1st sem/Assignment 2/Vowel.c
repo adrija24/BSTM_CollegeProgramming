59188d1b9f30f856ce01ef46e45ff20a1aa5b927
adrija24 <24adrijaroy@gmail.com>
2021-11-12 21:59:44 +0530

    Better code updated

--- 1st sem/Assignment 2/Vowel.c
+++ 1st sem/Assignment 2/Vowel.c
@@ -6,30 +6,13 @@ int main()
     char charac;
     printf("Enter a character: \n");
     scanf("%c", &charac);
-    if (charac == 'a')
+    if (charac == 'a' || charac == 'e'|| charac == 'i' || charac == 'o' || charac == 'u')
     {
        printf("This is a vowel");
-    }
-     else if (charac == 'e')
-    {
-       printf("This is a vowel");
-    }
-    else if (charac == 'i')
-    {
-        printf("This is a vowel");
-    }
-    else if (charac == 'o')
-    {
-        printf("This is a vowel");
-    }
-    else if (charac == 'u')
-    {
-        printf("This is a vowel");
     }
     else
     {
         printf("This is a consonant");
     }
-    
     return 0;
 }
