5a0d06c23b0e40b6a514a13710dc618bd731b1e1
adrija24 <24adrijaroy@gmail.com>
2021-11-14 22:26:50 +0530

    Bug fixed

--- 1st sem/Assignment 2/Vowel.c
+++ 1st sem/Assignment 2/Vowel.c
@@ -6,7 +6,7 @@ int main()
     char charac;
     printf("Enter a character: \n");
     scanf("%c", &charac);
-    if (charac == 'a' || charac == 'e'|| charac == 'i' || charac == 'o' || charac == 'u')
+    if (charac == 'a' || charac == 'e'|| charac == 'i' || charac == 'o' || charac == 'u'||charac == 'A' || charac == 'E'|| charac == 'I' || charac == 'O' || charac == 'U')
     {
        printf("This is a vowel");
     }
