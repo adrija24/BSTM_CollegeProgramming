f8101e238d66c7390711400c342cc626ab0b3105
adrija24 <24adrijaroy@gmail.com>
2021-12-06 22:45:37 +0530

    Factorial number

---
+++ 1st sem/Assignment 6/Factorial_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int fact(int a);
+void main(){
+int f=0, r=0;
+printf("enter digit:");
+scanf("%d",&f);
+r=fact(f);
+printf("result:%d", r);
+}
+int fact(int f){
+int l,n=1;
+for(l=1;l<=f;l++)
+{
+    n=n*l;
+
+}
+return n;
+
+}
